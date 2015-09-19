062357682c57fea0141e364eaaa6826a83ccda5c
Doug Cox <dougcoxs1@gmail.com>
2015-09-19 10:58:55 -0700

    Simple blockchain parser with no error handling.

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+extern crate byteorder;
+
+use std::fs::{File};
+use std::io::{Read};
+use byteorder::{LittleEndian, ReadBytesExt};
+
+fn main() {
+    let mut fh = File::open("../../blk00000.dat").unwrap();
+     
+    loop {
+        
+        let magic_id = fh.read_u32::<LittleEndian>().unwrap();
+        let header_length = fh.read_u32::<LittleEndian>().unwrap();
+        
+        println!("magic_id: {:x}", magic_id);
+        println!("header_length: {}", header_length);
+        
+        let mut buf = vec![0; header_length as usize];
+        let _ = fh.read(&mut buf);
+
+    }
+}
